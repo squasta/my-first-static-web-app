80e7245518ae8d7c6de4a705e6161272c2e49636
Stanislas Quastana <squastana@hotmail.com>
2020-06-29 16:26:54 +0200

    demo JR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello JR {{value}}</div>`,
 })
 export class AppComponent {
   value = 'Ma demo en live';
